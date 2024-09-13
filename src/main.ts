9e7adb794bf6586ff0e0442635df926ef4e120fe
Genilson Peixoto de Miranda <gmiranda@PSG.LOCAL>
2024-09-13 15:04:14 -0400

    configuração ssl

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors(); // Habilita CORS para permitir acesso externo
+//   await app.listen(8080, '192.168.1.127'); // Escuta na interface de rede especificada (IP) e porta 139
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000); // Escuta em todas as interfaces de rede
+  const httpsOptions = {
+    key: fs.readFileSync('ssl/chave-privada.pem'), // Caminho para a chave privada gerada
+    cert: fs.readFileSync('ssl/certificado.pem'), // Caminho para o certificado gerado
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  app.enableCors();
+  await app.listen(8080, '192.168.1.127'); // Escuta na porta 8080
 }
 bootstrap();
